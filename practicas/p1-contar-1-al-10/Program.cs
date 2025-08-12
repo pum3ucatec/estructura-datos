fa1d098d90a0cd1d710d9c77a92ba32abd4370b4
alerivera05-ux <alerivera.v05@gmail.com>
2025-08-12 19:42:39 -0400

    Agregando mi primer ejercicio

---
+++ practicas/p1-contar-1-al-10/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program
+{
+    public static void Main()
+    {
+        int i = 1;
+        while (i <= 10)
+        {
+            Console.WriteLine(i);
+            i++;
+        }
+    }
+}
