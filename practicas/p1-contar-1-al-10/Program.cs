1e27aacccabf6caddc7c1b9e23667f20ef394098
NotZayrix <abran_jhair@hotmail.com>
2025-08-13 19:50:01 -0400

    Ejercicio 1

---
+++ practicas/p1-contar-1-al-10/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Programa
+{
+    static void Main()
+    {
+        // Contar del 1 al 10 usando un bucle for
+        for (int i = 1; i <= 10; i++)
+        {
+            Console.WriteLine(i);
+        }
+
+        Console.WriteLine("Presiona cualquier tecla para salir...");
+        Console.ReadKey();
+    }
+}
