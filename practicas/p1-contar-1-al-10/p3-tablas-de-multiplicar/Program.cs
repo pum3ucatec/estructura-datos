5d3a0e45db8b96b6b3bc5be3cbb5335bf10fba02
RaulOzk4R <raul.lazarte@gmail.com>
2025-08-12 23:41:22 -0400

    Contar del 1 al 10 con bucle for en C#

---
+++ practicas/p1-contar-1-al-10/p3-tablas-de-multiplicar/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    public static void Main()
+    {
+        Console.Write("Ingrese un número para ver su tabla de multiplicar: ");
+        int numero = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine($"Tabla de multiplicar del {numero}:");
+
+        for (int i = 1; i <= 10; i++)
+        {
+            int resultado = numero * i;
+            Console.WriteLine($"{numero} x {i} = {resultado}");
+        }
+    }
+}
