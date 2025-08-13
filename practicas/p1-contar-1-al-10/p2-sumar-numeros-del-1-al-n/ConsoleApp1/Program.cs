03165dc79c78be9d35cef1c535ebaa9c8fd31465
RaulOzk4R <raul.lazarte@gmail.com>
2025-08-12 22:39:01 -0400

    Contar del 1 al 10 con bucle for en C#

---
+++ practicas/p1-contar-1-al-10/p2-sumar-numeros-del-1-al-n/ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    public static void Main()
+    {
+        Console.Write("Ingrese un número N: ");
+        int N = Convert.ToInt32(Console.ReadLine());
+
+        int suma = 0;
+        int contador = 1;
+
+        while (contador <= N)
+        {
+            suma += contador;
+            contador++;
+        }
+
+        Console.WriteLine($"La suma de los números del 1 al {N} es: {suma}");
+    }
+}
