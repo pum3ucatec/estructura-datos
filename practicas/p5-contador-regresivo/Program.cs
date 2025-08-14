440714b48093dd430758d2fd1b6ad6ada31eb989
NotZayrix <abran_jhair@hotmail.com>
2025-08-14 18:36:36 -0400

    Ejercicio 5

---
+++ practicas/p5-contador-regresivo/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Programa
+{
+    static void Main()
+    {
+        int numero = 10;
+
+        while (numero >= 1)
+        {
+            Console.WriteLine(numero);
+            numero--;
+        }
+
+        Console.WriteLine("Cuenta regresiva finalizada");
+        Console.WriteLine("Presiona cualquier tecla para salir.");
+        Console.ReadKey();
+    }
+}
