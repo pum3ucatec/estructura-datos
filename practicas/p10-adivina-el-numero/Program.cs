892f6a479f192618ba2d2db08e90d60c27a832e7
NotZayrix <abran_jhair@hotmail.com>
2025-08-14 18:55:55 -0400

    Ejercicio 10

---
+++ practicas/p10-adivina-el-numero/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+class Programa
+{
+    static void Main()
+    {
+        Random random = new Random();
+        int numeroSecreto = random.Next(1, 51); 
+        int intento;
+        int contadorIntentos = 0;
+
+        Console.WriteLine("Bienvenido al juego de Adivina el Numero");
+        Console.WriteLine("He pensado un número entre 1 y 50. ¿a que no adivinas cual es?\n");
+
+        do
+        {
+            Console.Write("Ingresa tu intento: ");
+            string? entrada = Console.ReadLine();
+            contadorIntentos++;
+
+            if (!int.TryParse(entrada, out intento))
+            {
+                Console.WriteLine("Entrada invalida. Debes ingresar un número entero.");
+                continue;
+            }
+
+            if (intento < numeroSecreto)
+            {
+                Console.WriteLine("El numero secreto es mayor.");
+            }
+            else if (intento > numeroSecreto)
+            {
+                Console.WriteLine("El numero secreto es menor.");
+            }
+            else
+            {
+                Console.WriteLine($"\nAdivinaste el numero {numeroSecreto} en {contadorIntentos} intentos.");
+            }
+
+        } while (intento != numeroSecreto);
+
+        Console.WriteLine("Presiona cualquier tecla para salir.");
+        Console.ReadKey();
+    }
+}
