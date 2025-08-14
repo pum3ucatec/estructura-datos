67a45c9bb55c5242479a5f5d87722dd81878e822
NotZayrix <abran_jhair@hotmail.com>
2025-08-14 18:44:38 -0400

    Ejercicio 6

---
+++ practicas/p6-validar-numero-positivo/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Programa
+{
+    static void Main()
+    {
+        int numero;
+
+        
+        do
+        {
+            Console.Write("Ingresa un número positivo: ");
+            string? entrada = Console.ReadLine();
+
+            
+            if (!int.TryParse(entrada, out numero))
+            {
+                Console.WriteLine("Entrada no valida. Debes ingresar un numero entero.");
+                numero = -1; 
+            }
+            else if (numero <= 0)
+            {
+                Console.WriteLine("El numero debe ser positivo.");
+            }
+
+        } while (numero <= 0);
+
+        Console.WriteLine($"Numero ingresado correctamente: {numero}");
+        Console.WriteLine("Presiona cualquier tecla para salir.");
+        Console.ReadKey();
+    }
+}
