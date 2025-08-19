1839f619aee1f347dd7ed77b8cf9565bae309aa7
RaulOzk4R <raul.lazarte@gmail.com>
2025-08-18 23:31:34 -0400

    Anadiendo practica2

---
+++ tabla-hash/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        Dictionary<string, int> edades = new Dictionary<string, int>();
+        edades["Juan"] = 25;
+        edades["Ana"] = 30;
+
+        Console.WriteLine(edades["Ana"]); // 30
+    }
+}
