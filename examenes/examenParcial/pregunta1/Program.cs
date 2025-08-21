74f9a4323994c2f814c053ec75f6836ac71bbfd1
edwinquisbertm <edwinquisbert.m@gmail.com>
2025-08-21 19:26:24 -0400

    Primer Examen Parcial

---
+++ examenes/examenParcial/pregunta1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class Program
+{
+    public static void Main()
+    {
+        int[] arreglo = { 1, 2, 3, 4, 5 };
+        int k = 2;
+
+        int[] rotado = RotarDerecha(arreglo, k);
+
+        Console.WriteLine("Arreglo rotado:");
+        Console.Write("[");
+        for (int i = 0; i < rotado.Length; i++)
+        {
+            Console.Write(rotado[i]);
+            if (i < rotado.Length - 1) Console.Write(", ");
+        }
+        Console.WriteLine("]");
+    }
+
+    public static int[] RotarDerecha(int[] arreglo, int k)
+    {
+        int n = arreglo.Length;
+        int[] resultado = new int[n];
+        k = k % n;
+
+        for (int i = 0; i < n; i++)
+        {
+            int nuevaPosicion = (i + k) % n;
+            resultado[nuevaPosicion] = arreglo[i];
+        }
+
+        return resultado;
+    }
+}
---
+++ examenes/examenParcial/pregunta2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+class Program
+{
+    public static void Main()
+    {
+        int[,] matriz = {
+            { 1, 2, 3 },
+            { 4, 5, 6 }
+        };
+
+        int[,] transpuesta = TransponerMatriz(matriz);
+
+        Console.WriteLine("Matriz transpuesta:");
+        for (int i = 0; i < transpuesta.GetLength(0); i++)
+        {
+            Console.Write("[ ");
+            for (int j = 0; j < transpuesta.GetLength(1); j++)
+            {
+                Console.Write(transpuesta[i, j] + " ");
+            }
+            Console.WriteLine("]");
+        }
+    }
+
+    public static int[,] TransponerMatriz(int[,] matriz)
+    {
+        int filas = matriz.GetLength(0);
+        int columnas = matriz.GetLength(1);
+        int[,] resultado = new int[columnas, filas];
+
+        for (int i = 0; i < filas; i++)
+        {
+            for (int j = 0; j < columnas; j++)
+            {
+                resultado[j, i] = matriz[i, j];
+            }
+        }
+
+        return resultado;
+    }
+}
