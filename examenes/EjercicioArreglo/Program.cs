342c96f4f5db9e793fe1f4022bfc7e6e46d19b04
alerivera05-ux <alerivera.v05@gmail.com>
2025-08-21 20:12:16 -0400

    Subo ejercicios 2 y 3 del examen parcial

---
+++ examenes/EjercicioArreglo/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        int[] arr = { 3, 6, 9, 12, 15, 18, 21 };
+        int n = arr.Length;
+        Console.WriteLine("Ingrese el valor de k para mover el arreglo: ");
+        int k = int.Parse(Console.ReadLine()!);
+        Console.WriteLine($"Elegiste:{k} ");
+
+        k = k % n;
+        int[] resultado = new int[n];
+        for (int i = 0; i < k; i++)
+        {
+            resultado[i] = arr[n - k + i];
+        }
+        for (int i = k; i < n; i++)
+        {
+            resultado[i] = arr[i - k];
+        }
+        Console.WriteLine("Arreglo original: " + string.Join(", ", arr));
+        Console.WriteLine("Arreglo rotado: " + string.Join(", ", resultado));
+    }
+}
---
+++ examenes/EjercicioMatriz/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Matriz de ejemplo
+        int[,] matriz = {
+            {5, 4, 3},
+            {9, 8, 7}
+        };
+
+        int filas = matriz.GetLength(0);
+        int columnas = matriz.GetLength(1);
+
+        // Crear la matriz transpuesta
+        int[,] transpuesta = new int[columnas, filas];
+
+        for (int i = 0; i < filas; i++)
+        {
+            for (int j = 0; j < columnas; j++)
+            {
+                transpuesta[j, i] = matriz[i, j];
+            }
+        }
+
+        // Mostrar la matriz original
+        Console.WriteLine("Matriz original:");
+        for (int i = 0; i < filas; i++)
+        {
+            for (int j = 0; j < columnas; j++)
+            {
+                Console.Write(matriz[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
+
+        Console.WriteLine();
+
+        // Mostrar la matriz transpuesta
+        Console.WriteLine("Matriz transpuesta:");
+        for (int i = 0; i < columnas; i++)
+        {
+            for (int j = 0; j < filas; j++)
+            {
+                Console.Write(transpuesta[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
+
